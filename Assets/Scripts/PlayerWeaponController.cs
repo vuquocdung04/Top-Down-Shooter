d2d121ee1b69a057c86be4870439b8f368da9155
Dung <vuquocdung204@gmail.com>
2025-11-19 11:22:08 +0700

    bullet direction

--- Assets/Scripts/PlayerWeaponController.cs
+++ Assets/Scripts/PlayerWeaponController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class PlayerWeaponController : MonoBehaviour
@@ -8,6 +9,9 @@ public class PlayerWeaponController : MonoBehaviour
     [SerializeField] private float bulletSpeed;
     [SerializeField] private Transform gunPoint;
 
+    [SerializeField] private Transform weaponHolder;
+    [SerializeField] private Transform aim;
+    
     private void Start()
     {
         player = GetComponent<Player>();
@@ -16,11 +20,29 @@ public class PlayerWeaponController : MonoBehaviour
 
     private void Shoot()
     {
+        
         GameObject newBullet =
             Instantiate(bulletPrefab, gunPoint.position, Quaternion.LookRotation(gunPoint.forward));
         
-        newBullet.GetComponent<Rigidbody>().velocity = gunPoint.forward * bulletSpeed;
+        newBullet.GetComponent<Rigidbody>().velocity = BulletDirection() * bulletSpeed;
         Destroy(newBullet, 10f);
         GetComponentInChildren<Animator>().SetTrigger("Fire");
+    }
+
+    private Vector3 BulletDirection()
+    {
+        Vector3 direction = (aim.position - gunPoint.position).normalized;
+        direction.y = 0;
+        weaponHolder.LookAt(aim);
+        gunPoint.LookAt(aim);
+        
+        return direction;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(weaponHolder.position, weaponHolder.position + weaponHolder.forward * 25f);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(gunPoint.position, gunPoint.position + BulletDirection() * 25f);
     }
 }
