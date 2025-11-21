5c56034097045a95ec2444ad7c6f52f642371af0
Dung <vuquocdung204@gmail.com>
2025-11-21 21:10:52 +0700

    cp

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,11 +1,23 @@
-using System;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    [SerializeField] private GameObject bulletImpactFX;
     private Rigidbody rb => GetComponent<Rigidbody>();
     private void OnCollisionEnter(Collision other)
     {
-        rb.constraints = RigidbodyConstraints.FreezeAll;
+        CreateImpactFX(other);
+        Destroy(gameObject);
+    }
+
+    private void CreateImpactFX(Collision other)
+    {
+        if (other.contacts.Length > 0)
+        {
+            ContactPoint contact = other.contacts[0];
+            GameObject newImpactFX = Instantiate(bulletImpactFX, contact.point, Quaternion.LookRotation(contact.normal));
+            
+            Destroy(newImpactFX, 1f);
+        }
     }
 }
