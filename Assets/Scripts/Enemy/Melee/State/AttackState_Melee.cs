f423f15f11021cbd27d7bc2f37f9752766fb2d02
Dung <vuquocdung204@gmail.com>
2025-11-27 11:43:26 +0700

    multiple slash attacks

--- Assets/Scripts/Enemy/Melee/State/AttackState_Melee.cs
+++ Assets/Scripts/Enemy/Melee/State/AttackState_Melee.cs
@@ -24,6 +24,8 @@ public class AttackState_Melee : EnemyState
         attackMoveSpeed = enemy.attackData.moveSpeed;
         enemy.anim.SetFloat("AttackAnimationSpeed", enemy.attackData.animationSpeed);
         enemy.anim.SetFloat("AttackIndex", enemy.attackData.attackIndex);
+        enemy.anim.SetFloat("SlashAttackIndex", Random.Range(0,6));
+        
 
         enemy.agent.isStopped = true;
         enemy.agent.velocity = Vector3.zero;
