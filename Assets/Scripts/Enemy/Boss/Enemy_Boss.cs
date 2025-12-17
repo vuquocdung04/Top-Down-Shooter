9c93230a4871db547d9afafcf7f7655d6839a595
Dung <vuquocdung204@gmail.com>
2025-12-17 21:39:26 +0700

    boss's jump attack damage check

--- Assets/Scripts/Enemy/Boss/Enemy_Boss.cs
+++ Assets/Scripts/Enemy/Boss/Enemy_Boss.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public enum BossWeaponType
@@ -8,55 +9,49 @@ public enum BossWeaponType
 
 public class Enemy_Boss : Enemy
 {
-    [Header("Boss Details")]
-    public BossWeaponType bossWeaponType;
+    [Header("Boss Details")] public BossWeaponType bossWeaponType;
     public float actionCooldown = 10;
     public float attackRange;
 
-    [Header("Ability - Flame and Hummer")]
-    [Tooltip("nhỏ hơn dùng ability")]
+    [Header("Ability - Flame and Hummer")] [Tooltip("nhỏ hơn dùng ability")]
     public float minAbilityDistance;
+
     public float abilityCooldown;
     private float lastTimeUsedAbility;
-    
-    [Header("Flamethrower")]
-    public float flameDamageCooldown;
+
+    [Header("Flamethrower")] public float flameDamageCooldown;
     public ParticleSystem flameThrower;
     public float flameThrowDuration = 10;
     public bool flameThrowActive { get; private set; }
-    
-    [Header("Hummer")]
-    public GameObject activationPrefab;
-    [Space]
-    
-    [Header("Jump Attack")]
-    [Tooltip("lớn hơn thì jump")]
+
+    [Header("Hummer")] public GameObject activationPrefab;
+
+    [Space] [Header("Jump Attack")] [Tooltip("lớn hơn thì jump")]
     public float minJumpDistanceRequired;
-    
+
     public float travelTimeToTarget = 1;
     public float jumpAttackCooldown = 10;
     private float lastTimeJumped = -10f;
-    [Space]
-    public float impactRadius = 2.5f;
+    [Space] public float impactRadius = 2.5f;
     public float impactPower = 5;
     public Transform impactPoint;
     [SerializeField] private float upforceMultiplier = 10; // chi anh huong toi truc y
-    
+
     [Space] [SerializeField] private LayerMask whatToIgnore;
 
     [Header("Attack")] [SerializeField] private Transform[] damagePoints;
     [SerializeField] private float attackCheckRadius;
     [SerializeField] private GameObject meleeAttackFx;
-    
+
     public IdleState_Boss idleState { get; private set; }
     public MoveState_Boss moveState { get; private set; }
-    public AttackState_Boss attackState {get; private set;}
-    public JumpAttackState_Boss  jumpAttackState { get; private set; }
-    public AbilityState_Boss  abilityState { get; private set; }
-    public DeadState_Boss  deadState { get; private set; }
-    
+    public AttackState_Boss attackState { get; private set; }
+    public JumpAttackState_Boss jumpAttackState { get; private set; }
+    public AbilityState_Boss abilityState { get; private set; }
+    public DeadState_Boss deadState { get; private set; }
+
     public Enemy_BossVisuals bossVisuals { get; private set; }
-    
+
     protected override void Awake()
     {
         base.Awake();
@@ -68,7 +63,6 @@ public class Enemy_Boss : Enemy
         jumpAttackState = new JumpAttackState_Boss(this, stateMachine, "JumpAttack");
         abilityState = new AbilityState_Boss(this, stateMachine, "Ability");
         deadState = new DeadState_Boss(this, stateMachine, "Idle"); // Idle is just a placeholder we use ragdoll
-        
     }
 
     protected override void Start()
@@ -80,23 +74,23 @@ public class Enemy_Boss : Enemy
     protected override void Update()
     {
         base.Update();
-        
+
         stateMachine.currentState.UpdateState();
-        
+
         MeleeAttackCheck(damagePoints, attackCheckRadius, meleeAttackFx);
     }
 
     public override void Die()
     {
         base.Die();
-        if(stateMachine.currentState != deadState)
+        if (stateMachine.currentState != deadState)
             stateMachine.ChangeState(deadState);
     }
 
     public override void EnterBattleMode()
     {
-        if(inBattleMode)return;
-        
+        if (inBattleMode) return;
+
         base.EnterBattleMode();
         stateMachine.ChangeState(moveState);
     }
@@ -113,10 +107,10 @@ public class Enemy_Boss : Enemy
 
         var mainModule = flameThrower.main;
         var extraModule = flameThrower.transform.GetChild(0).GetComponent<ParticleSystem>().main;
-        
+
         mainModule.duration = flameThrowDuration;
         extraModule.duration = flameThrowDuration;
-        
+
         flameThrower.Clear();
         flameThrower.Play();
     }
@@ -124,14 +118,14 @@ public class Enemy_Boss : Enemy
     public void ActivateHummer()
     {
         GameObject newActivation = ObjectPool.instance.GetObject(activationPrefab, impactPoint);
-        ObjectPool.instance.ReturnObject(newActivation,1);
+        ObjectPool.instance.ReturnObject(newActivation, 1);
     }
-    
+
     public bool CanDoAbility()
     {
-        bool playerWithinDistance = Vector3.Distance(transform.position,player.position) < minAbilityDistance;
-        
-        if(playerWithinDistance == false) return false;
+        bool playerWithinDistance = Vector3.Distance(transform.position, player.position) < minAbilityDistance;
+
+        if (playerWithinDistance == false) return false;
         return Time.time > lastTimeUsedAbility + abilityCooldown;
     }
 
@@ -140,49 +134,72 @@ public class Enemy_Boss : Enemy
 
     public void JumpImpact()
     {
-        Transform imPoint = this.impactPoint;
-        if (imPoint == null)
-            imPoint = transform;
+        Transform impPoint = impactPoint;
+        if (impPoint == null)
+            impPoint = transform;
         
-        Collider[] colliders = Physics.OverlapSphere(imPoint.position, impactRadius);
+        MassDamage(impPoint.position, impactRadius);
+    }
 
-        foreach (var col in colliders)
+    private void MassDamage(Vector3 impPoint, float impRadius)
+    {
+        HashSet<GameObject> uniqueEntities = new();
+        Collider[] colliders = Physics.OverlapSphere(impPoint, impRadius, ~whatIsAlly);
+
+        foreach (var hit in colliders)
         {
-            Rigidbody rb = col.GetComponent<Rigidbody>();
-            if(rb != null)
-                rb.AddExplosionForce(impactPower, transform.position, impactRadius, upforceMultiplier, ForceMode.Impulse);
+            IDamageable damageable = hit.GetComponent<IDamageable>();
+
+            if (damageable != null)
+            {
+                GameObject rootEntity = hit.transform.root.gameObject;
+                if (uniqueEntities.Add(rootEntity) == false)
+                    continue;
+
+                damageable.TakeDamage();
+            }
+            ApplyPhysicalForceTo(impPoint, hit);
         }
     }
+
+    private void ApplyPhysicalForceTo(Vector3 impPoint, Collider hit)
+    {
+        Rigidbody rb = hit.GetComponent<Rigidbody>();
+        if (rb != null)
+            rb.AddExplosionForce(impactPower, impPoint, impactRadius, upforceMultiplier, ForceMode.Impulse);
+    }
+
     public bool CanDoJumpAttack()
     {
         float distanceToPlayer = Vector3.Distance(transform.position, player.position);
-        
-        if(distanceToPlayer < minJumpDistanceRequired)
+
+        if (distanceToPlayer < minJumpDistanceRequired)
             return false;
-        
+
         return Time.time > lastTimeJumped + jumpAttackCooldown && IsPlayerInClearSight();
     }
- 
+
     public void SetJumpAttackCooldown() => lastTimeJumped = Time.time;
+
     private bool IsPlayerInClearSight()
     {
-        Vector3 myPos = transform.position + new Vector3(0,1.5f,0);
+        Vector3 myPos = transform.position + new Vector3(0, 1.5f, 0);
         Vector3 playerPos = player.position + Vector3.up;
-        
+
         Vector3 directionToPlayer = (playerPos - myPos).normalized;
 
         if (Physics.Raycast(myPos, directionToPlayer, out RaycastHit hit, 100, ~whatToIgnore))
         {
-            if(hit.transform == player || hit.transform.parent == player)
+            if (hit.transform == player || hit.transform.parent == player)
                 return true;
         }
 
         return false;
     }
-    
+
     public bool PlayerInAttackRange() => Vector3.Distance(transform.position, player.position) < attackRange;
-    
-    
+
+
     protected override void OnDrawGizmos()
     {
         base.OnDrawGizmos();
@@ -191,25 +208,25 @@ public class Enemy_Boss : Enemy
 
         if (player != null)
         {
-            Vector3 myPos = transform.position + new Vector3(0,1.5f,0);
+            Vector3 myPos = transform.position + new Vector3(0, 1.5f, 0);
             Vector3 playerPos = player.position + Vector3.up;
             Gizmos.color = Color.magenta;
-            
+
             Gizmos.DrawLine(myPos, playerPos);
         }
-        
+
         Gizmos.color = Color.green;
         Gizmos.DrawWireSphere(transform.position, minJumpDistanceRequired);
-        
+
         Gizmos.color = Color.cyan;
         Gizmos.DrawWireSphere(transform.position, impactRadius);
 
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(transform.position, minAbilityDistance);
-        
+
         if (damagePoints.Length > 0)
         {
-            foreach(var damagePoint in damagePoints)
+            foreach (var damagePoint in damagePoints)
                 Gizmos.DrawWireSphere(damagePoint.position, attackCheckRadius);
         }
     }
