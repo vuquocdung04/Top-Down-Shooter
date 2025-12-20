dd35225498388608850c0deb7456cd9b4b534860
Dung <vuquocdung204@gmail.com>
2025-12-20 16:11:37 +0700

    cp

--- Assets/Scripts/LevelGeneration/LevelGenerator.cs
+++ Assets/Scripts/LevelGeneration/LevelGenerator.cs
@@ -1,28 +1,67 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class LevelGenerator : MonoBehaviour
 {
+    [SerializeField] private Transform lastLevelPart;
     [SerializeField] private List<Transform> levelParts;
+    private List<Transform> currentLevelParts;
     [SerializeField] private SnapPoint nextSnapPoint;
+
+    [Space] [SerializeField] private float generationCooldown;
+
+    private float coolDownTimer;
+    private bool generationOver;
+
+    private void Start()
+    {
+        currentLevelParts = levelParts;
+    }
+
+    private void Update()
+    {
+        coolDownTimer -= Time.deltaTime;
+        if (coolDownTimer < 0)
+        {
+            if (currentLevelParts.Count > 0)
+            {
+                coolDownTimer = generationCooldown;
+                GenerateNextLevelPart();
+            }
+            else if (generationOver == false)
+            {
+                FinishGeneration();
+            }
+        }
+    }
+
+    private void FinishGeneration()
+    {
+        generationOver = true;
+
+        Transform levelPart = Instantiate(lastLevelPart);
+        LevelPart levelPartScript = levelPart.GetComponent<LevelPart>();
+        
+        levelPartScript.SnapAndAlignPartTo(nextSnapPoint);
+    }
 
     [ContextMenu("Create next level part")]
     private void GenerateNextLevelPart()
     {
         Transform newPart = Instantiate(ChooseRandomPart());
         LevelPart levelPartScript = newPart.GetComponent<LevelPart>();
-        
+
         levelPartScript.SnapAndAlignPartTo(nextSnapPoint);
         nextSnapPoint = levelPartScript.GetExitPoint();
     }
-    
-    
+
     private Transform ChooseRandomPart()
     {
-        int randomIndex = Random.Range(0, levelParts.Count);
-        Transform choosePart = levelParts[randomIndex];
-        levelParts.RemoveAt(randomIndex);
+        int randomIndex = Random.Range(0, currentLevelParts.Count);
+        Transform choosePart = currentLevelParts[randomIndex];
+        currentLevelParts.RemoveAt(randomIndex);
         return choosePart;
     }
-
 }
