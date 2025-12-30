3c29e6905b80f6457145c6bf14dc7bf8dca7faff
Dung <vuquocdung204@gmail.com>
2025-12-30 21:27:32 +0700

    ui buttons

---
+++ Assets/Scripts/UI/UI_Button.cs
@@ -0,0 +1,48 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class UI_Button : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [Header("Mouse hover settings")] public float scaleSpeed = 1;
+    public float scaleRate = 1.2f;
+    private Vector3 defaultScale;
+    private Vector3 targetScale;
+    
+    private Image buttonImage;
+    private TextMeshProUGUI buttonText;
+    private void Start()
+    {
+        defaultScale = transform.localScale;
+        targetScale = transform.localScale;
+        
+        buttonImage = GetComponent<Image>();
+        buttonText = GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    private void Update()
+    {
+        if (Mathf.Abs(transform.lossyScale.x - targetScale.x) > 0.01f)
+        {
+            float scaleValue = Mathf.Lerp(transform.localScale.x, targetScale.x, Time.deltaTime * scaleSpeed);
+            
+            transform.localScale = Vector3.one *  scaleValue;
+        }
+    }
+
+    public virtual void OnPointerEnter(PointerEventData eventData)
+    {
+        targetScale = defaultScale * scaleRate;
+        buttonImage.color = Color.yellow;
+        buttonText.color = Color.yellow;
+    }
+
+    public virtual void OnPointerExit(PointerEventData eventData)
+    {
+        targetScale = defaultScale;
+        
+        buttonImage.color = Color.white;
+        buttonText.color = Color.white;
+    }
+}
